4ecd020ecd874a0efd9f719cda8d7cca1b051476
HowCam <3604402972@qq.com>
2024-03-28 13:00:50 +0800

    添加了注释

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import openpyxl
 class FloatingWindow:
     def __init__(self, master):
         self.master = master
-        master.title("点名器")
-        master.attributes("-topmost", True)  # 窗口置顶
+        master.title("点名器")   # 设置窗口标题
+        master.attributes("-topmost", True)  # 窗口保持置顶
 
         # 设置窗口图标
-        icon = Image.open("icon.png")  # 替换为你的png文件路径
+        icon = Image.open("icon.png")  # png文件路径
         master.iconphoto(True, ImageTk.PhotoImage(icon))
 
         self.load_names_from_excel()
 
-        self.label = tk.Label(master, font=("楷体", 120), fg="#005f35")
+        self.label = tk.Label(master, font=("楷体", 120), fg="#005f35")  #设定字体样式
         self.label.place(relx=0.5, rely=0.5, anchor=tk.CENTER)  # 将标签放置在窗口中央
 
         self.update_name()
@@ -27,11 +27,12 @@ class FloatingWindow:
 
     def load_names_from_excel(self):
         self.names = []
-        wb = openpyxl.load_workbook('names.xlsx')  # 替换为你的xlsx文件路径
+        wb = openpyxl.load_workbook('names.xlsx')  # xlsx文件路径
         ws = wb.active
         for row in ws.iter_rows(values_only=True):
             self.names.extend(row)
 
+#   #选择随机姓名
     def update_name(self, event=None):
         name = random.choice(self.names)
         self.label.config(text=name)
