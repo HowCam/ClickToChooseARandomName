a6432f538aeb6ba0983bc8b544ea0aa7fb4e38f1
HowCam <3604402972@qq.com>
2024-03-28 18:36:39 +0800

    1.1.2
    修复了已知问题

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ class FloatingWindow:
         master.title("点名器")   # 设置窗口标题
         master.geometry("650x300+100+100")  # 设置窗口大小和位置
 
+        # 设置图标
+        master.iconbitmap("icon.ico")
+
         # 存储当前版本号
-        self.current_version = "1.1.1"
+        self.current_version = "1.1.2"
 
         # 获取远程服务器上的最新版本号
         self.latest_version = self.get_latest_version()
@@ -25,16 +28,16 @@ class FloatingWindow:
         self.update_name()
 
         self.name_label.bind("<Button-1>", self.update_name)  # 点击窗口更新姓名
-        self.master.bind("<space>", self.update_name)    #按下空格键更新姓名
+        self.master.bind("<space>", self.update_name)     #按下空格键更新姓名
         self.master.bind("<Return>", self.update_name)    #按下回车键更新姓名
-        self.master.bind("<Down>", self.update_name)    #按下方向下键更新姓名
+        self.master.bind("<Down>", self.update_name)      #按下方向下键更新姓名
 
         # 添加全局键盘监听事件
         self.shift_pressed = False
-        keyboard.on_press_key("shift", self.toggle_window)
+        keyboard.on_press_key("x", self.toggle_window)
 
         # 添加提示文本
-        self.tip_label = tk.Label(master, text="按下 Shift 键隐藏窗口", font=("仿宋", 12), fg="gray")
+        self.tip_label = tk.Label(master, text="按下 X 键隐藏/显示窗口", font=("仿宋", 12), fg="gray")
         self.tip_label.place(relx=1.0, rely=1.0, anchor=tk.SE)
 
         # 显示当前版本
@@ -43,6 +46,9 @@ class FloatingWindow:
 
         # 检查更新并显示通知
         self.check_and_notify_update()
+
+        # 设置窗口置顶
+        master.attributes("-topmost", True)
 
     def load_names_from_excel(self):
         self.names = []
@@ -55,6 +61,7 @@ class FloatingWindow:
     def update_name(self, event=None):
         name = random.choice(self.names)
         self.name_label.config(text=name)
+        self.master.update()  # 更新 GUI
 
     # 切换隐藏/显示窗口状态
     def toggle_window(self, event):
@@ -77,12 +84,13 @@ class FloatingWindow:
     # 检查更新并显示通知
     def check_and_notify_update(self):
         if self.latest_version and self.latest_version != self.current_version:
-            message = f"发现新版本 {self.latest_version}，请前往 GitHub 下载！"
+            message = f"发现新版本 {self.latest_version}，请 前往GitHub 或 联系作者 获取最新版本！"
             messagebox.showinfo("更新提示", message)
 
 def main():
     root = tk.Tk()
     app = FloatingWindow(root)
+    app.update_name()  # 初始化时更新一次姓名
     root.mainloop()
 
 if __name__ == "__main__":
